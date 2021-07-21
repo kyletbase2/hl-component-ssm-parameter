101ef264fe25089a1bddc29aa3780a80ff15dd18
Kyle Thorne <k.thorne@base2services.com>
2021-07-21 22:31:00 +1000

    Added ability to generate random value

--- ssm-parameter.cfndsl.rb
+++ ssm-parameter.cfndsl.rb
@@ -7,6 +7,11 @@ CloudFormation do
   ssm_parameters =  external_parameters.fetch(:ssm_parameters, {})
 
   ssm_parameters.each do |name, ssm_param|
+
+    if ssm_param.has_key?('random') && ssm_param('random')
+      ssm_param['value'] = (0...12).map { (65 + rand(26)).chr }.join
+    end
+    
     SSM_Parameter("#{name.gsub('-','').gsub('_','')}Parameter") do
       Type 'String'
       Name FnSub(ssm_param['name'])
